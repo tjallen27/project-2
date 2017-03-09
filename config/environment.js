3633017ca21c6266744dd965c5166544bb564cc3
tjallen27 <tjallen91@hotmail.co.uk>
2017-03-09 16:38:34 +0000

    updated environment config

--- config/environment.js
+++ config/environment.js
@@ -1,6 +1,6 @@
 const port = process.env.PORT || 3000;
 const env = process.env.NODE_ENV || 'development';
-const dbURI = process.env.MONGO_URI || `mongodb://localhost/project2-${env}`;
+const dbURI = process.env.MONGODB_URI || `mongodb://localhost/project2-${env}`;
 const sessionSecret = process.env.SESSION_SECRET || 'my awesome secret';
 
 module.exports = { port, env, dbURI, sessionSecret };
